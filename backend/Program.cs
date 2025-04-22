a85e5cf047d4842b8c626c114d4903821352d77f
Jakub Jurczyk <pandetthe@gmail.com>
2025-04-22 12:27:14 +0200

    Remove swagger, add config that enums are provided as strings

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,6 +9,8 @@ using Serilog;
 using Serilog.Events;
 using System.Reflection;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Boardly.Backend;
 
@@ -42,6 +44,8 @@ public class Program
             builder.Services.AddSingleton<BoardService>();
 
             builder.Services.AddControllers();
+            builder.Services.ConfigureHttpJsonOptions(options =>
+                options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false)));
 
             builder.Services.AddSingleton<TokenService>();
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -71,10 +75,6 @@ public class Program
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
-                app.UseSwaggerUI(options =>
-                {
-                    options.SwaggerEndpoint("/openapi/v1.json", "Boardly API V1");
-                });
                 app.MapScalarApiReference(options =>
                 {
                     options.WithHttpBearerAuthentication(new HttpBearerOptions());
