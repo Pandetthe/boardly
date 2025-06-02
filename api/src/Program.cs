543fb01751b130341abd356a31d481abab3a31ff
Jakub Jurczyk <pandetthe@gmail.com>
2025-06-02 22:19:38 +0200

    Add jwt to hub

--- api/src/Program.cs
+++ api/src/Program.cs
@@ -89,6 +89,20 @@ public class Program
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]
                             ?? throw new NullReferenceException("Jwt key must be provided!")))
                     };
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (!string.IsNullOrEmpty(accessToken) &&
+                                (path.StartsWithSegments("/chathub")))
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
                 });
             builder.Services.AddOpenApi(options =>
             {
