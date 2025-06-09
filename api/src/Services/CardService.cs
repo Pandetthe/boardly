81e1cd9e9bc2913bd350a839919a579601e2cfac
Jakub Jurczyk <pandetthe@gmail.com>
2025-06-09 20:38:30 +0200

    Optimize MoveCardAsync db requests

--- api/src/Services/CardService.cs
+++ api/src/Services/CardService.cs
@@ -5,6 +5,7 @@ using Boardly.Api.Models.Dtos;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using System.Linq;
 
 namespace Boardly.Api.Services;
 
@@ -333,7 +334,6 @@ public class CardService
             throw new ForbidenException("One or more assigned users are not members of this board.");
         card.UpdatedAt = DateTime.UtcNow;
         var update = Builders<Card>.Update
-            .Set(c => c.SwimlaneId, card.SwimlaneId)
             .Set(c => c.ListId, card.ListId)
             .Set(c => c.Title, card.Title)
             .Set(c => c.Description, card.Description)
@@ -346,20 +346,27 @@ public class CardService
 
     public async Task MoveCardAsync(ObjectId cardId, ObjectId userId, ObjectId listId, CancellationToken cancellationToken = default)
     {
-        Card card = await _cardsCollection.Find(x => x.Id == cardId).FirstOrDefaultAsync(cancellationToken)
-                    ?? throw new RecordDoesNotExist("Card has not been found.");
-        Board board = await _boardsCollection.Find(x => x.Id == card.BoardId).FirstOrDefaultAsync(cancellationToken) 
-                      ?? throw new RecordDoesNotExist("Board has not been found.");
-        Member member = board.Members.FirstOrDefault(x => x.UserId == userId) 
-                        ?? throw new ForbidenException("User is not a member of this board.");
-        if (member.Role == BoardRole.Viewer && !card.AssignedUsers.Contains(member.UserId))
-        {
+        var result = await _cardsCollection
+            .AsQueryable()
+            .Where(card => card.Id == cardId)
+            .Join(
+                _boardsCollection,
+                c => c.BoardId,
+                b => b.Id,
+                (c, b) => new
+                {
+                    CardAssignedUsers = c.AssignedUsers,
+                    BoardMembers = b.Members
+                }
+            )
+            .FirstOrDefaultAsync(cancellationToken)
+            ?? throw new RecordDoesNotExist("Card has not been found");
+        var member = result.BoardMembers.FirstOrDefault(m => m.UserId == userId);
+        if (member?.Role == BoardRole.Viewer && !result.CardAssignedUsers.Contains(userId))
             throw new ForbidenException("User does not have permission to move this card");
-        }
-        var filter = Builders<Card>.Filter.Eq(x => x.Id, cardId);
         var update = Builders<Card>.Update
             .Set(c => c.ListId, listId);
-        await _cardsCollection.UpdateOneAsync(filter, update, cancellationToken: cancellationToken);
+        await _cardsCollection.UpdateOneAsync(x => x.Id == cardId, update, cancellationToken: cancellationToken);
     }
 
     public async Task DeleteCardAsync(ObjectId cardId, ObjectId userId, CancellationToken cancellationToken = default)
