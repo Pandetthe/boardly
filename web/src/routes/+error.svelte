24e2ec8830f03c01f9c35d442e1822bc1a40f98c
Jakub Jurczyk <pandetthe@gmail.com>
2025-06-15 21:37:38 +0200

    Add error page

---
+++ web/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from "$app/state";
+</script>
+
+<svelte:head>
+  <title>Error {page.status}</title>
+</svelte:head>
+<div class="flex flex-col items-center gap-2 justify-center min-h-screen text-center px-4 w-full">
+  {#if page.status === 404}
+    <h1 class="text-6xl font-bold">Page Not Found</h1>
+    <p class="text-gray-600 text-lg">The page you are looking for does not exist.</p>
+  {:else}
+    <h1 class="text-6xl font-bold">An Error Occurred</h1>
+    <p class="text-gray-600 text-lg">Something went wrong. Please try again later.</p>
+  {/if}
+</div>
